6947087c2ced6283471a737506f544a87bf7eceb
justtherightsize <147991029+justtherightsize@users.noreply.github.com>
2023-10-16 16:45:08 +0200

    Add files via upload

--- inference/2stage.py
+++ inference/2stage.py
@@ -2,35 +2,53 @@ import numpy as np
 import torch
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
 
-# Prepare input texts. This model is pretrained on Czech data 
-test_texts = ['Utterance1;Utterance2;Utterance3']
+
+# Prepare input texts. This model is fine-tuned for Czech.
+test_texts = ['utterance1;utterance2;utterance3']
 
 # Load the model and tokenizer
-model = AutoModelForSequenceClassification.from_pretrained(
-    'justtherightsize/robeczech-2stage-online-risks-cs', num_labels=5).to("cuda")
-
-tokenizer = AutoTokenizer.from_pretrained(
-    'justtherightsize/robeczech-2stage-online-risks-cs',
-    use_fast=False, truncation_side='left')
-assert tokenizer.truncation_side == 'left'
+bin_model_name = 'justtherightsize/small-e-czech-binary-online-risks-cs'
+stage2_model_name = 'justtherightsize/small-e-czech-2stage-online-risks-cs'
+bin_model = AutoModelForSequenceClassification.from_pretrained(
+    bin_model_name, num_labels=2).to("cuda")
+stage2_model = AutoModelForSequenceClassification.from_pretrained(
+    stage2_model_name, num_labels=5).to("cuda")
+bin_tokenizer = AutoTokenizer.from_pretrained(
+    bin_model_name, use_fast=False, truncation_side='left')
+stage2_tokenizer = AutoTokenizer.from_pretrained(
+    stage2_model_name, use_fast=False, truncation_side='left')
+assert bin_tokenizer.truncation_side == 'left'
+assert stage2_tokenizer.truncation_side == 'left'
 
 # Define helper functions
-def predict_one(text: str, tok, mod, threshold=0.5):
-    encoding = tok(text, return_tensors="pt", truncation=True, padding=True,
-                   max_length=256)
-    encoding = {k: v.to(mod.device) for k, v in encoding.items()}
-    outputs = mod(**encoding)
-    logits = outputs.logits
-    sigmoid = torch.nn.Sigmoid()
-    probs = sigmoid(logits.squeeze().cpu())
+def get_preds_bin(text, tokenizer, model):
+    """
+    Return the predicted classes
+    """
+    inputs = tokenizer(text, padding=True, truncation=True, max_length=256,
+                       return_tensors="pt").to("cuda")
+    outputs = model(**inputs)
+    probs = outputs[0].softmax(1)
+    preds = torch.argmax(probs, -1).detach().cpu()
+    return preds
+
+def get_preds_multi(text, tokenizer, model):
+    inputs = tokenizer(text, padding=True, truncation=True, max_length=256,
+                       return_tensors="pt").to("cuda")
+    outputs = model(**inputs)
+    probs = outputs[0].softmax(1)
     predictions = np.zeros(probs.shape)
-    predictions[np.where(probs >= threshold)] = 1
-    return predictions, probs
+    predictions[np.where(probs.detach().cpu() >= 0.5)] = 1
+    return predictions
 
-def print_predictions(texts):
-    preds = [predict_one(tt, tokenizer, model) for tt in texts]
-    for c, p in preds:
-        print(f'{c}: {p.tolist():.4f}')
+bin_decoded = get_preds_bin(test_texts, bin_tokenizer, bin_model)
+stage2_decoded = get_preds_multi(test_texts, stage2_tokenizer, stage2_model)
 
-# Run the prediction
-print_predictions(test_texts)
+preds_combined = []
+for b,m in zip(bin_decoded, stage2_decoded):
+    if b == 0:
+        preds_combined.append([1,0,0,0,0,0])
+    else:
+        preds_combined.append([0] + m.astype(int).tolist())
+
+print(preds_combined)
